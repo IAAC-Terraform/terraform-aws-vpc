6e72a259c45f2f714915001ebd611ce6c45603f6
Raghavan R <raghavan.r@capgemini.com>
2018-08-17 13:19:52 +1000

    exporting outputs within the VPC module

---
+++ output.tf
@@ -0,0 +1,27 @@
+output "vpcid" {
+  value = "${module.vpc.id}"
+}
+
+output "vpcarn" {
+  value = "${module.vpc.vpcarn}"
+}
+
+output "vpc-cidr" {
+  value = "${module.vpc.vpc-cidr}"
+}
+
+output "main-rt" {
+  value = "${module.vpc.main-rt}"
+}
+
+output "default-nacl" {
+  value = "${module.vpc.default-nacl}"
+}
+
+output "default-sg" {
+  value = "${module.vpc.default-sg}"
+}
+
+output "default-rt" {
+  value = "${module.vpc.default-rt}"
+}
