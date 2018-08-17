fcdf90817303731dea225d178ee7a2e310f584ae
Raghavan R <raghavan.r@capgemini.com>
2018-08-17 12:54:59 +1000

    variable fix

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
     source = "./modules/vpc/"
     name       = "${var.vpc_name}"
-    cidr_block = "${var.cidr}"
+    cidr_block = "${var.cidr_block}"
     env        = "${var.env}"
     create_vpc = "${var.create_vpc}"
 }
--- variable.tf
+++ variable.tf
@@ -1,4 +1,4 @@
-variable "name" {
+variable "vpc_name" {
   default = ""
 }
 
